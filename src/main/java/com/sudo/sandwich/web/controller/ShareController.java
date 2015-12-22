18216d674e829258d0213dcc08d8c5b1c50e5eac
Sitaram Guruswamy <sguruswamy@gmail.com>
2015-12-22 15:22:07 -0500

    use post to call the share controller

--- src/main/java/com/sudo/sandwich/web/controller/ShareController.java
+++ src/main/java/com/sudo/sandwich/web/controller/ShareController.java
@@ -13,6 +13,7 @@ import com.sudo.sandwich.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class ShareController {
     @Inject
     UserService userService;
 
-    @RequestMapping("/screenshare")
+    @RequestMapping(value="/screenshare", method={RequestMethod.POST,RequestMethod.GET})
     public
     @ResponseBody
     JsonNode shareScreen(String ntid) {
