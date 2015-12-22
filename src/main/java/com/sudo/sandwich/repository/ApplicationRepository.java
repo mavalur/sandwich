7061308d85fe7cf7df52fcbde396454d0f6f73fa
Satish Terala <satishkt@gmail.com>
2015-12-21 22:35:49 -0500

    Added query dsl support for all Mongo entities and for the repositories.

--- src/main/java/com/sudo/sandwich/repository/ApplicationRepository.java
+++ src/main/java/com/sudo/sandwich/repository/ApplicationRepository.java
@@ -2,11 +2,12 @@ package com.sudo.sandwich.repository;
 
 import com.sudo.sandwich.json.domain.Application;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by satishterala on 12/21/15.
  */
 @Repository
-public interface ApplicationRepository extends MongoRepository<Application,String>{
+public interface ApplicationRepository extends MongoRepository<Application,String>,QueryDslPredicateExecutor<Application>{
 }
--- src/main/java/com/sudo/sandwich/repository/GroupRepository.java
+++ src/main/java/com/sudo/sandwich/repository/GroupRepository.java
@@ -3,6 +3,7 @@ package com.sudo.sandwich.repository;
 import com.sudo.sandwich.json.domain.Group;
 import com.sudo.sandwich.json.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import org.springframework.stereotype.Repository;
  * Created by satishterala on 12/21/15.
  */
 @Repository
-public interface GroupRepository extends MongoRepository<Group,String> {
+public interface GroupRepository extends MongoRepository<Group,String>,QueryDslPredicateExecutor<Group> {
 }
