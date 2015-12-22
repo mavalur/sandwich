a29ec04e820a33cd6c30c2560200f6eeb1925d06
Satish Terala <satishkt@gmail.com>
2015-12-22 00:12:50 -0500

    Added chat statement stuff, parsing for tags and other stuff.

--- src/main/java/com/sudo/sandwich/json/domain/ChatStatement.java
+++ src/main/java/com/sudo/sandwich/json/domain/ChatStatement.java
@@ -6,6 +6,7 @@ import com.twitter.Extractor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -34,6 +35,7 @@ public class ChatStatement {
     @Version
     Long version;
 
+    @Transient
     Extractor extractor;
 
     @Indexed
