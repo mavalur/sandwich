0366f52df4093e96ecd8072df8f466858c4bbabb
Sitaram Guruswamy <sguruswamy@gmail.com>
2015-12-22 01:17:01 -0500

    Added a controller method to get onCallUsers

--- src/main/java/com/sudo/sandwich/web/controller/UserController.java
+++ src/main/java/com/sudo/sandwich/web/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.sudo.sandwich.web.controller;
 
 import com.sudo.sandwich.json.domain.User;
+import com.sudo.sandwich.services.OnCallService;
 import com.sudo.sandwich.services.UserService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
 
@@ -21,8 +24,11 @@ public class UserController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
     @Inject
-    UserService userService;
+    OnCallService onCallService;
+    
 
+    @Inject
+    UserService userService;
     @RequestMapping("/sudo/user")
     public
     @ResponseBody
@@ -30,4 +36,10 @@ public class UserController {
         return userService.getUser(userId);
     }
 
+    @RequestMapping("/onCall")
+    public ModelAndView getOnCallUsers(@PathVariable String groupId) {
+    	ModelAndView mv = new ModelAndView();
+    	mv.addObject("users", onCallService.getOnCallUser(groupId));
+    	return mv;
+    }
 }
