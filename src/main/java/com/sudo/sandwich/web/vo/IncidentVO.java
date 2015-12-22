4c30aaca38808b11ebdb78e61e63528cf607c6b0
Satish Terala <satishkt@gmail.com>
2015-12-22 13:31:08 -0500

    Refactored theme stuff.

---
+++ src/main/java/com/sudo/sandwich/web/vo/IncidentVO.java
@@ -0,0 +1,21 @@
+package com.sudo.sandwich.web.vo;
+
+import com.sudo.sandwich.json.domain.Incident;
+
+import java.util.Collection;
+
+/**
+ * Created by satishterala on 12/22/15.
+ */
+public class IncidentVO {
+
+    public int openIssues;
+
+    public int overDueIssues;
+
+    public int noOfIssues;
+
+    Collection<Incident> incidents;
+
+
+}
