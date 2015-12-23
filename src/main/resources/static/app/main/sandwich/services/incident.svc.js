9ec3f1978ef1c4997efb70be0a019e81f740401b
Mavalur, Harikumar <HMavalur@wellington.com>
2015-12-23 14:39:31 -0500

    non committed files committing

--- src/main/resources/static/app/main/sandwich/services/incident.svc.js
+++ src/main/resources/static/app/main/sandwich/services/incident.svc.js
@@ -1,26 +1,23 @@
-(function ()
-{
-    'use strict';
+(function() {
+	'use strict';
 
-    angular
-        .module('app.sandwich')
-        .factory('IncidentService', IncidentService);
+	angular.module('app.sandwich').factory('IncidentService', IncidentService);
 
-    /** @ngInject */
-    function IncidentService($window,$http)
-    {
-            return {
-                initiateBridge:function{
-                            var data = $.param({
-                                json: JSON.stringify({
-                                    incidentId: 'INC1234',
-                                    numbers:'+15083536038,+18623362045'
-                                })
-                            });
-                            $http.post("https://a6891304.ngrok.io/broadcast/send", data).success(function(data, status) {
-                                $scope.hello = data;
-                            })
-                };
-            }
-    }
+	/** @ngInject */
+	function IncidentService($window, $http) {
+		return {
+			initiateBridge : function() {
+				var data = $.param({
+					json : JSON.stringify({
+						incidentId : 'INC1234',
+						numbers : '+15083536038,+18623362045'
+					})
+				});
+				$http.post("https://a6891304.ngrok.io/broadcast/send", data)
+						.success(function(data, status) {
+							$scope.hello = data;
+						});
+			}
+		}
+	}
 }());
